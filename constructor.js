b2836bcac6368865cf73d76472dce3376466604a
Diana Marcelin <marcelindiana@yahoo.com>
2023-08-13 13:26:16 -0400

    prototype inheritance

--- constructor.js
+++ constructor.js
@@ -13,8 +13,24 @@ User.prototype.logout = function () {
   console.log(this.email, "has logged out");
 };
 
+///args places admin information (email,name) into an array
+function Admin(...args) {
+  //takes User function and allows it to be used in Admin function
+  User.apply(this, args);
+  this.role = "super admin";
+}
+Admin.prototype = Object.create(User.prototype);
+
+Admin.prototype.deleteUser = function (u) {
+  users = users.filter((user) => {
+    return user.email != u.email;
+  });
+};
+
 let userOne = new User("danny@ninjas.com", "Danny");
 let userTwo = new User("adam@ninjas.com", "Adam");
+let admin = new Admin("belle@ninjas.com", "Belle");
+let users = [userOne, userTwo, admin];
 
-console.log(userOne);
-userTwo.login();
+//admin function cont.
+console.log(admin);
