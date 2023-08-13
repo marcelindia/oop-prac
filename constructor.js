19efda7e2ad776fcca6a66e2b3153014e400d2c5
Diana Marcelin <marcelindiana@yahoo.com>
2023-08-13 13:12:28 -0400

    user prototype

--- constructor.js
+++ constructor.js
@@ -2,10 +2,16 @@ function User(email, name) {
   this.email = email;
   this.name = name;
   this.online = false;
-  this.login = function () {
-    console.log(this.email, "has logged in");
-  };
 }
+
+User.prototype.login = function () {
+  this.online = true;
+  console.log(this.email, "has logged in");
+};
+User.prototype.logout = function () {
+  this.online = false;
+  console.log(this.email, "has logged out");
+};
 
 let userOne = new User("danny@ninjas.com", "Danny");
 let userTwo = new User("adam@ninjas.com", "Adam");
