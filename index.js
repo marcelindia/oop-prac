aa2322ca81f9361cf9410aaea7e0a31616471488
Diana Marcelin <marcelindiana@yahoo.com>
2023-08-13 12:41:18 -0400

    created user class example

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-let userOne = {
-  email: "rye@ninjas.com",
-  name: "Rye",
-  //methods
-  login() {
-    console.log(this.email, "has logged in");
-  },
-  logout() {
-    console.log(this.email, "has logged out");
-  },
-};
+// let userOne = {
+//   email: "rye@ninjas.com",
+//   name: "Rye",
+//   //methods
+//   login() {
+//     console.log(this.email, "has logged in");
+//   },
+//   logout() {
+//     console.log(this.email, "has logged out");
+//   },
+// };
 
-console.log(userOne.name);
+// console.log(userOne.name);
+
+// //updating properties
+
+// userOne.name = "Diana";
+// userOne.age = 27;
+
+//classes
+class User {
+  constructor(email, name) {
+    this.email = email;
+    this.name = name;
+  }
+}
+
+let userOne = new User("danny@ninjas.com", "Danny");
+let userTwo = new User("adam@ninjas.com", "Adam");
+console.log(userOne);
+console.log(userTwo);
